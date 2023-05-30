52d77667921eb2877fbacb4aa93b217dada8e27a
Benoit Mariat <benoit.mariat@intersec.com>
2023-05-30 10:55:05 +0200

    setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	server: { https: true },
+	plugins: [
+		mkcert(),
+		VitePWA({ registerType: 'autoUpdate' }),
+		sveltekit(),
+	],
 });

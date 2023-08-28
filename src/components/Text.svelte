005f3e76901d17f4c296278769a962f782b6071b
Benoit Mariat <benoit.mariat@intersec.com>
2023-08-28 02:26:02 +0200

    Text: add tag to change text alignment

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -11,12 +11,21 @@
 
     type Chunk = {
         content: string | IconDesc;
-        type: 'text' | 'icon' | 'line-feed' | 'italic' | 'bold' | 'link';
+        type: 'text' | 'icon' | 'line-feed' | 'italic' | 'bold' | 'link' | 'justify' | 'center' | 'markdown';
         title?: string;
     };
 
     function splitText(rawText: string): Chunk[] {
-        const splittedText = rawText.split(/(:\w+:|[\n]|_\w[\s\S]*\w_|\*{2}\w[\s\S]*\w\*{2}|\[[^\]\n]+\]\([^)\n]+\))/);
+        /**
+         * :lineText: → icon
+         * \n → new line
+         * _text_ → italic
+         * **text** → bold
+         * [lineText](url) → url
+         * |justify|text|/justify| → justifed text
+         * |center|text|/center| → centered text
+        **/
+        const splittedText = rawText.split(/(:\w+:|[\n]|_\w[\s\S]*\w_|\*{2}\w[\s\S]*\w\*{2}|\[[^\]\n]+\]\([^)\n]+\)|\|justify\|[\s\S]+\|\/justify\||\|center\|[\s\S]+\|\/center\|)/);
         const chunks: Chunk[] = [];
 
         splittedText.forEach((str) => {
@@ -62,6 +71,28 @@
                         content: data?.groups?.text ?? '',
                     });
                 }
+            } else if (str.startsWith('|') && str.endsWith('|')) {
+                const data = str.match(/^\|(?<style>justify|center)\|(?<text>[\s\S]*)\|\/\1\|$/);
+                const {style, text} = data?.groups ?? {};
+
+                if (style === 'justify') {
+                    chunks.push({
+                        type: 'justify',
+                        content: text,
+                    });
+                } else if (style === 'center') {
+                    chunks.push({
+                        type: 'center',
+                        content: text,
+                    });
+                } else {
+                    /* unknown style */
+                    chunks.push({
+                        type: 'markdown',
+                        content: text,
+                    });
+                }
+
             } else {
                 if (str.trim()) {
                     chunks.push({
@@ -93,10 +124,20 @@
         <span class="text-bold">
             <svelte:self text={content} />
         </span>
+    {:else if type === 'justify'}
+        <p class="text-justify">
+            <svelte:self text={content} />
+        </p>
+    {:else if type === 'center'}
+        <p class="text-center">
+            <svelte:self text={content} />
+        </p>
     {:else if type === 'link'}
         <a target="_blank" href={title} rel="noreferrer">
             <svelte:self text={content} />
         </a>
+    {:else if type === 'markdown'}
+        <svelte:self text={content} />
     {/if}
 {/each}
 
@@ -106,5 +147,11 @@
 }
 .text-bold {
     font-weight: bold;
+}
+.text-justify {
+    text-align: justify;
+}
+.text-center {
+    text-align: center;
 }
 </style>
